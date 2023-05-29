43a5f0a2bf1fbf5432ce28f021e0cabefc0229a4
eugen <eugen.kudraschow@gmail.com>
2023-05-29 20:14:03 +0200

    Homepage created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import '../css/HomePage.css'
+import glogo from '../assets/g-logo.png'
 
 export const HomePage = () => {
   return (
-    <div>
-        <div>HomePage</div>
-        <div>Have fun Eugen</div>
+    <div className="homepage_container">
+        <h1>Länderspiel (aber nicht {'\u26BD'})</h1>
+        <h2>Jetzt einloggen</h2>
+        <div className="login">
+          <button className="gbutton">
+            <img src={glogo} alt="google-logo" width="18px"/>
+            <span>Mit Google anmelden</span>
+          </button>
+          <button className="guestbutton">
+            <span>als Gast spielen</span>
+          </button>  
+        </div>
     </div>
   )
 }
