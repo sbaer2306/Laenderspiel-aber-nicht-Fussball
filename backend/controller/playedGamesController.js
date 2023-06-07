235eb365199b7b698133c660e8e02a28184183ac
Johannes Matus <>
2023-06-07 14:21:12 +0200

    isPrivate bug fix (falsch negiert)

--- backend/controller/playedGamesController.js
+++ backend/controller/playedGamesController.js
@@ -27,7 +27,7 @@ const getPlayedGames = async (req, res) => {
         }
 
         // user profile may be undefined --> treat as private
-        if (!user.Profile || !user.Profile.isPrivate) {
+        if (!user.Profile || user.Profile.isPrivate) {
             return res.status(403).json({ message: 'Unauthorized - the profile is set to private, therefore the game history is private too.' });
         }
 
