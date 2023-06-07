c1ebc42ff38a069230b6895909b1b2c4de4e8659
Johannes Matus <>
2023-06-07 17:40:57 +0200

    Game History table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import {LoggedHomePage} from './pages/LoggedHomePage'
 import SwaggerDocDisplay from './components/ApiDoc/SwaggerDocDisplay'
 import Navbar from './components/UI/Navbar'
 import Ranking from './components/Ranking/Ranking'
+import GameHistory from './components/profile/GameHistory'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage/>} />
           <Route path='/ranking' element={<Ranking/>} />
+          <Route path='/user/:id/gamehistory' element={<GameHistory/>} />
           <Route path='/logged' element={<LoggedHomePage/>} />
           <Route path='/api/doc' element={<SwaggerDocDisplay/>} />
           <Route path='/game/:game_id/facts' element={<FirstRound/>} />
---
+++ frontend/src/components/profile/GameHistory.jsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, Button, Text, Divider } from '@chakra-ui/react';
+import { formatDate } from '../../helpers/date';
+import { useParams } from 'react-router-dom';
+
+function GameHistory() {
+    const { id } = useParams();
+    const user_id = id;
+
+  const [gameHistory, setGameHistory] = useState([]);
+  const [nextPage, setNextPage] = useState(null);
+  const [prevPage, setPrevPage] = useState(null);
+  const [isPrivate, setIsPrivate] = useState(false);
+
+  useEffect(() => {
+    fetchGameHistory();
+  }, []);
+
+  const fetchGameHistory = async (page = 1, pageSize = 5) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8000/user/${user_id}/played-games?page=${page}&pageSize=${pageSize}`
+      );
+
+      if (response.status === 403) {
+        console.error('Game history is private.');
+        setIsPrivate(true);
+        return;
+      }
+
+      const data = await response.json();
+      setGameHistory(data.playedGames);
+      setNextPage(data._links.next?.href);
+      setPrevPage(data._links.prev?.href);
+    } catch (error) {
+      console.error('Error fetching game history:', error);
+    }
+  };
+
+  const handleLoadNext = () => {
+    if (nextPage) {
+      const pageRegex = /page=(\d+)/;
+      const match = pageRegex.exec(nextPage);
+      const nextPageNumber = match ? parseInt(match[1]) : null;
+      if (nextPageNumber) {
+        fetchGameHistory(nextPageNumber);
+      }
+    }
+  };
+
+  const handleLoadPrev = () => {
+    if (prevPage) {
+      const pageRegex = /page=(\d+)/;
+      const match = pageRegex.exec(prevPage);
+      const prevPageNumber = match ? parseInt(match[1]) : null;
+      if (prevPageNumber) {
+        fetchGameHistory(prevPageNumber);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Text fontSize='lg' fontWeight='bold'>Game History</Text>
+    {isPrivate ? <Text fontSize='md'>This user has set their profile to private. 🔒</Text> : (
+        <><Divider my={2} />
+      <Table variant="simple" size='sm'>
+        <Thead>
+          <Tr>
+            <Th>Country</Th>
+            <Th>Score</Th>
+            <Th>Duration</Th>
+            <Th>Created At</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {gameHistory.map((game) => (
+            <Tr key={game.id}>
+              <Td fontWeight='semibold'>{game.country.name}</Td>
+              <Td>{game.score}</Td>
+              <Td>{game.gameDuration} seconds</Td>
+              <Td>{formatDate(game.createdAt)}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+      <div>
+        {prevPage && <Button my={2} mx={10} colorScheme='green' onClick={handleLoadPrev}>Load previous</Button>}
+        {nextPage && <Button my={2} mx={10} colorScheme='blue' onClick={handleLoadNext}>Load next</Button>}
+      </div>
+        </>
+    )} 
+    </div>
+  );
+}
+
+export default GameHistory;
