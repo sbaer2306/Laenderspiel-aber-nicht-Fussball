c003c3bbf249ee9d2e9bf76cfb90242d27e1a840
Johannes Matus <>
2023-06-09 11:14:51 +0200

    Fixed Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,8 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage/>} />
           <Route path='/ranking' element={<Ranking/>} />
-          <Route path='/user/:id/public-profile' element={<GameHistory/>} />
+          <Route path='/user/:id/public-profile' element={<PublicProfileOverview/>} />
+          <Route path='/user/profile' element={<PrivateProfileOverview/>} />
           <Route path='/logged' element={<LoggedHomePage/>} />
           <Route path='/api/doc' element={<SwaggerDocDisplay/>} />
           <Route path='/game/:game_id/facts' element={<FirstRound/>} />
