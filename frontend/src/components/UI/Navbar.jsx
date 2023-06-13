cf24485e9029bed7fd9f5525d7ad62c3f875250a
Johannes Matus <>
2023-06-13 16:29:20 +0200

    Navbar angepasst

--- frontend/src/components/UI/Navbar.jsx
+++ frontend/src/components/UI/Navbar.jsx
@@ -1,7 +1,28 @@
 import { Link } from 'react-router-dom';
 import { Button, Flex, Text } from '@chakra-ui/react';
+import { useUserAuth } from '../../hooks/userAuthContext';
+import { useNavigate } from 'react-router-dom';
+import { useToast } from '@chakra-ui/react';
 
 function Navbar() {
+
+    const navigate = useNavigate();
+    const toast = useToast();
+
+    const { logout, userIsLoggedIn } = useUserAuth();
+
+    const handleLogout = () => {
+        logout();
+        toast({
+            title: "Logged out!",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+            position: "top"
+        });
+        navigate('/');
+    }
+
     return (
         <><Flex alignItems="center" justifyContent="space-between" px={4} py={2}>
             <Flex alignItems="center">
@@ -20,6 +41,21 @@ function Navbar() {
                         API Documentation
                     </Button>
                 </Link>
+                {userIsLoggedIn() && 
+                (
+                <>
+                <Link to="/user/profile">
+                    <Button colorScheme="blue" variant={'outline'} mr={2}>
+                        My Profile
+                    </Button>
+                </Link>
+                    <Button colorScheme="red" variant={'outline'} mr={2} onClick={handleLogout}>
+                        Logout
+                    </Button>
+                </>
+                )
+                
+                }
             </Flex>
         </Flex></>
         
