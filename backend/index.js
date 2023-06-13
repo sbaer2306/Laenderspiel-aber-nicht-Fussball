43c64bac1d10cacb9ea45b21343e2ec53cab5356
Johannes Matus <>
2023-06-13 16:17:57 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,12 @@ prisma.initializePrisma();
 require('./config/passport')(passport)
 app.use(passport.initialize())
 
-// CORS-Header setzen
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization, If-None-Match',
+  credentials: true,
+}));
 
 // Include the auth routes
 const authRoutes = require('./routes/authRoutes');
