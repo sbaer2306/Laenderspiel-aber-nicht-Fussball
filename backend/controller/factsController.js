1ce4522265ba57720a9725e7548787a21c4b5e1d
EuKud <eugen.kudraschow@gmail.com>
2023-06-29 17:23:14 +0200

    redis stuff facts

--- backend/controller/factsController.js
+++ backend/controller/factsController.js
@@ -3,14 +3,18 @@ const prisma = require('../prisma/prisma');
 const prismaClient = prisma.getPrisma();
 
 async function getFacts(req, res){
-  const userID = req.user.id;
-  const gameID = req.params;
-  const redisClient = req.redis;
+  
   try {
-    console.log("redisclient: ", redisClient);
+    const userID = req.user.id;
+    const {id} = req.params;
+    const redisClient = req.redis;
+
     //Game from redis
-    const game = await redisClient.hget(gameID, 'games');
+    const gameString = await redisClient.hget(id, 'games');
+    const game = JSON.parse(gameString);
+
     console.log("game from db: ", game);
+    console.log("gameuser", game.user_id);
     if(!game) return res.status(404).json({error: "Game not found"})
     if( game.user_id !== userID){
       return res.status(403).json({error: "Forbidden. User is not player of the game.", game: game, user_id: userID})
--- backend/controller/geoController.js
+++ backend/controller/geoController.js
@@ -11,33 +11,32 @@ const prismaClient = prisma.getPrisma();
  * @throws {Error} - If an error occurs during the process.
  */
 async function getOsmData(req, res){
-    try{
-      //ID from url
-      const {id} = req.params;
-      
-      //Game from session
-      const game = req.session.game;
-      const userID = game.user_id;
+  const {id} = req.params;
+  const userID = game.user_id;
+  const redisClient = req.redis;
 
-      if(!game) return res.status(404).json({error: "Game not found"});
+  try{
+     const game = await redisClient.hget(id, 'games');
 
-      if(userID !== req.user.id) return res.status(403).json({error: "Forbidden. User is not player of the game."});
+     //if(!game) return res.status(404).json({error: "Game not found"});
+
+    //if(userID !== req.user.id) return res.status(403).json({error: "Forbidden. User is not player of the game."});
+  
+
+    const country = await prismaClient.country.findUnique({
+      where: {
+        id: Number(game.country_id),
+      }
+    })
+
+    const osmData = await geoService.fetchOsmData(country.name);
+    const center = geoService.getCenterOfCountry(osmData.elements[0].members);
     
-
-      const country = await prismaClient.country.findUnique({
-        where: {
-          id: Number(game.country_id),
-        }
-      })
-
-      const osmData = await geoService.fetchOsmData(country.name);
-      const center = geoService.getCenterOfCountry(osmData.elements[0].members);
-      
-      req.session.game = game;
-      res.status(200).json({geometry: osmData, center: center});
-    }catch(error){
-        res.status(500).json({error: 'Internal server error'});
-    }
+    req.session.game = game;
+    res.status(200).json({geometry: osmData, center: center});
+  }catch(error){
+      res.status(500).json({error: 'Internal server error'});
+  }
 }
 
 module.exports = {getOsmData}
